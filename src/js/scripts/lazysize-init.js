98d68f92cbde5b6d6be57444a5013bcd75ea1074
Alex Prisyazhnyuk <prisyaj98@gmail.com>
2019-04-23 02:29:12 +0300

    Update lazysize-init.js

--- src/js/scripts/lazysize-init.js
+++ src/js/scripts/lazysize-init.js
@@ -1,16 +1,26 @@
-import lazySizes from 'lazysizes'
-import lazybgset from 'lazysizes/plugins/bgset/ls.bgset' // eslint-disable-line
-import 'lazysizes/plugins/respimg/ls.respimg' /* webpackChunkName: "picturePolyfill" */
+/**
+ * Lazy Loading
+ */
+import lazySizes from 'lazysizes';
+import 'lazysizes/plugins/bgset/ls.bgset';
+import 'lazysizes/plugins/respimg/ls.respimg';
 
 const lazySizeInit = () => {
+  document.addEventListener('lazyloaded', (e) => {
+    const target = e.target.parentNode;
+    if (target) {
+      target.style.backgroundColor = 'transparent';
+    }
+  });
+
   // Lasysizes Lazyload Config
-  const lazySizesConfig = window.lazySizesConfig || {}
-  window.lazySizesConfig = lazySizesConfig
-  window.lazySizesConfig.expand = 130
-  lazySizesConfig.expFactor = 1.3
+  const lazySizesConfig = window.lazySizesConfig || {};
+  window.lazySizesConfig = lazySizesConfig;
+  window.lazySizesConfig.expand = 500; // default 360-500
+  lazySizesConfig.expFactor = 1.7; // default: 1.7
 
   // Lazy Sizes Init
-  lazySizes.init()
-}
+  lazySizes.init();
+};
 
-export default lazySizeInit
+export default lazySizeInit;
